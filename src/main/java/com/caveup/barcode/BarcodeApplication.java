79fdb3962ae6d462954e7c9ff512b2abaeb857d8
xw80329 <xliangwu@gmail.com>
2022-01-09 20:59:52 +0800

    #init project#

---
+++ src/main/java/com/caveup/barcode/BarcodeApplication.java
@@ -0,0 +1,16 @@
+package com.caveup.barcode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author xw80329
+ */
+@SpringBootApplication
+public class BarcodeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BarcodeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/caveup/barcode/BarcodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.caveup.barcode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BarcodeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
