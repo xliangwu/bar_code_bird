cb536dbe8d587515a29e7c2ded006781beb83b07
xw80329 <xliangwu@gmail.com>
2022-01-26 21:29:44 +0800

    #add FE codes#

---
+++ src/main/java/com/caveup/barcode/controller/IndexController.java
@@ -0,0 +1,18 @@
+package com.caveup.barcode.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @author xw80329
+ */
+@Controller
+public class IndexController {
+
+    private static final String DEFAULT_INDEX_PAGE = "index";
+
+    @RequestMapping(value = {"/*", "/index"})
+    public String greeting() {
+        return DEFAULT_INDEX_PAGE;
+    }
+}
